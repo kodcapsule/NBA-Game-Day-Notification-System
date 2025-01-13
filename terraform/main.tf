0d60c2a213a67619449b1579c3e5423c96e21109
kodcapsule <kusehsw@gmail.com>
2025-01-13 17:32:16 +0000

    added sns policy to resouce to terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -25,8 +25,39 @@ resource "aws_sns_topic" "gd_topic" {
 resource "aws_sns_topic_subscription" "email-sub" {
     topic_arn = aws_sns_topic.gd_topic.arn
     endpoint = "kusehsw@gmail.com"
-    protocol = "email"
-   
-    
+    protocol = "email"     
+  
+}
+
+resource "aws_iam_policy" "name" {
+  name = "gd_sns_policy"
+
+   policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "sns:Publish",
+        ]
+        Effect   = "Allow"
+        Resource = "arn:aws:sns:${var.region}:${var.account_id}:gd_topic"
+      },
+    ]
+  })
+  
+}
+
+
+variable "region" {
+  type = string
+  description = "Default region for my code"
+  default = "us-east-1"
+  
+}
+
+variable "account_id" {
+  type = string
+  description = "Default account id for practice"
+  default = "641638335125"
   
 }
